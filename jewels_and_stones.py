50938b5fd82f67c6cafdea9e96908d5fd4943cdc
tadchompson <tad.chompson@gmail.com>
2018-03-15 09:52:32 -0700

    Create jewels_and_stones.py

---
+++ jewels_and_stones.py
@@ -0,0 +1,27 @@
+"""
+You're given strings J representing the types of stones that are jewels, 
+and S representing the stones you have.  Each character in S is a type of
+stone you have.  You want to know how many of the stones you have are also
+jewels.
+
+The letters in J are guaranteed distinct, and all characters in J and S
+are letters. Letters are case sensitive, so "a" is considered a different
+type of stone from "A".
+"""
+
+class Solution(object):
+    def numJewelsInStones(self, J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+        """
+        dic = {}
+        dic[J] = 0
+        for chars in S:
+            if chars in J:
+                dic[J] +=1
+        return dic[J]
+        """
+        return sum(map(J.count, S))
